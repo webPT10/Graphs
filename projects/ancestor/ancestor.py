9de10de0085b685820685978fd132a83105b58d9
Eric Ferguson <ferguson.eric.r@gmail.com>
2020-10-18 17:45:12 -0700

    ancestor - Graph class

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -17,6 +17,18 @@ class Queue():
     def size(self):
         return len(self.queue)
 
+class Graph():
+    def __init__(self):
+        self.verts = {} # adj list (dictionary) of vertices mapping labels to edges
+
+    def add_vertex(self, vertex_id):
+        if vertex_id not in self.verts:
+            self.verts[vertex_id] = set()
+    
+    def add_edge(self, v1, v2):
+        if v1 in self.verts and v2 in self.verts:
+            self.verts[v1].add(v2)
+
 
 def earliest_ancestor(ancestors, starting_node):
     pass
