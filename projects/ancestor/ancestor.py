a95112b646d2e1a23f33c960af54ca83ed29d42b
Eric Ferguson <ferguson.eric.r@gmail.com>
2020-10-18 17:42:53 -0700

    ancestor - Queue class

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,22 @@
+# looking for earliest ancestor so need a DFS
+# need a Queue and Graph class
+
+class Queue():
+    def __init__(self):
+        self.queue = []
+
+    def enqueue(self, value):
+        self.queue.append(value)
+    
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+    
+    def size(self):
+        return len(self.queue)
+
 
 def earliest_ancestor(ancestors, starting_node):
     pass
